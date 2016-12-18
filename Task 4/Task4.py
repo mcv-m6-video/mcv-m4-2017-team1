215ba99f93d0f48e8b744e6ced2cae0e65b0f909
ArantxaCasanova <ar.casanova.8@gmail.com>
2016-12-18 20:32:28 +0100

    Task4 double problem

--- Task 4/Task4.py
+++ Task 4/Task4.py
@@ -5,12 +5,13 @@ import numpy as np
 def flow_read(filename):
     # loads flow field F from png file
 
-    I = np.double(np.uint8(cv2.imread(filename)))
-    F=I
-    print(I)
+    I = cv2.imread(filename)
+    I=I.astype('double')
+    F = I
 
-    F_u = np.double((I[:, :, 0]-2**8 ) / 64.0)
-    F_v = np.double((I[:, :, 1]-2**8 )  / 64.0)
+    print(F)
+    F_u = (I[:, :, 0]-2**15 ) / 64.0
+    F_v = (I[:, :, 1]-2**15 )  / 64.0
     F_valid = np.minimum(I[:, :, 2], 1.0)
     F_u[F_valid == 0] = 0
     F_v[F_valid == 0] = 0
