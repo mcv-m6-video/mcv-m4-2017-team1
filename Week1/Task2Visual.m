2ea7fbda94d0372ca05f865887caa9fe2d975174
bluque <luquelopez.belen@gmail.com>
2016-12-20 18:30:26 +0100

    task4 and 2

--- Week1/Task2Visual.m
+++ Week1/Task2Visual.m
@@ -1,18 +1,24 @@
 clearvars;
+InputDirectory = '../datasets/highway/reducedinput/';
 TestDirectory = '../results/highway/testB/';
 GTDirectory = '../datasets/highway/reducedGT/';
 NFrames = 200;
 
+FilesInput = dir(strcat(InputDirectory, '*jpg'));
 FilesTest = dir(strcat(TestDirectory, '*png'));
 FilesGT = dir(strcat(GTDirectory, '*png'));
-
-FMeasureTotal = [];    
-for k = 0:5:25
+FMeasureTotal = [];
+    
+F(NFrames) = struct('cdata',[],'colormap',[]);
+for k = 0 %0:5:25
     precision = zeros(1,200); recall = zeros(1,200); FMeasure = zeros(1,200);
-
+    v = VideoWriter(strcat(int2str(k),'desynch.avi'));
+    v.FrameRate = 10;
+    open(v)
     TPTotal=0;FPTotal=0;TNTotal=0;FNTotal=0;
     for i = 1:length(FilesTest)-k
         j=i+k;
+        realImage = imread(strcat(InputDirectory, FilesInput(i).name));
         testImage = double(imread(strcat(TestDirectory, FilesTest(i).name)));
         gtImage = double(imread(strcat(GTDirectory, FilesGT(j).name))) >= 170; 
         
@@ -22,13 +28,24 @@ for k = 0:5:25
         TNTotal=TNTotal+TN;
         FNTotal=FNTotal+FN;
         [precision(i),recall(i),accuracy,FMeasure(i)] = computeMetrics(TP,FP,TN,FN);
+        
+        figure(3)  
+        subplot(2,2,1); imshow(realImage); title('Input image')
+        subplot(2,2,2); imshow(testImage); title('Result from Test A')
+        subplot(2,2,[3,4]); plot(1:i,FMeasure(1:i),'b'); title('F Measure');
+        xlabel('# frame'); ylabel('F Measure')
 
+        axis([0 200 0 1])
+        drawnow(); 
+        F(i) = getframe(gcf);
+        writeVideo(v,F(i));
     end
     [~,~,~,FMeasureT] = computeMetrics(TPTotal,FPTotal,TNTotal,FNTotal);
     FMeasureTotal = [FMeasureTotal FMeasureT];
-    figure(1); plot(FMeasure)
-    drawnow(); hold on;
-
+    %figure(1); plot(FMeasure)
+    %drawnow(); hold on;
+    
+    close(v)
 end
-    figure(2); plot(FMeasureTotal); 
+    %figure(2); plot(FMeasureTotal); 
 
--- Week1/computeMetrics.m
+++ Week1/computeMetrics.m
@@ -1,11 +1,13 @@
 function [precision,recall,accuracy,FMeasure] = computeMetrics(TP,FP,TN,FN)
 
 precision = TP / (TP+FP);
-precision(isnan(precision)) = 0;
 recall = TP / (TP+FN);
-recall(isnan(recall)) = 0;
 accuracy = (TP+TN)/(TP+TN+FP+FN);
 FMeasure = 2*precision*recall / (precision+recall);
+
+%Change NaN's (which appear when dividing by 0) for 0s
+precision(isnan(precision)) = 0;
+recall(isnan(recall)) = 0;
 FMeasure(isnan(FMeasure)) = 0;
 
 end
